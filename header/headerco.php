c54ca38f1541b1c818ae441c8833947084d22341
Mathis Boulais <https://github.com/Asukyy>
2023-05-19 14:31:34 +0200

    head

--- header/headerco.php
+++ header/headerco.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="css/header.css">
 
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -15,7 +15,7 @@
 
         <div class="innerco">
         <div class="nooone">
-                <img class="headerco" src="../images/conservatoirelogo.png" alt="logo" >
+                <img class="headerco" src="images/conservatoirelogo.png" alt="logo" >
             </div>
         </div>
 
